0106612ae695ab622d52073863e01a3c41228d3b
HuaHungy <2024213272@bupt.cn>
2025-10-25 17:38:41 +0800

    unitree_g1_replay_fix

--- src/robocoin_dataset/sim_replay/configs/unitree_g1_config.py
+++ src/robocoin_dataset/sim_replay/configs/unitree_g1_config.py
@@ -88,15 +88,32 @@ class UnitreeG1Dof29ThreeFingerHandConfig(LerobotSimReplayConfig):
     state_gripper_joint_mjcf_names: list[str] = []
     action_gripper_joint_mjcf_names = state_gripper_joint_mjcf_names
 
-    state_gripper_lerobot_names: list[str] = []
+    state_gripper_lerobot_names: list[str] = [
+        # left_hand
+        "left_hand_joint_1_rad",
+        "left_hand_joint_2_rad",
+        "left_hand_joint_3_rad",
+        "left_hand_joint_4_rad",
+        "left_hand_joint_5_rad",
+        "left_hand_joint_6_rad",
+        "left_hand_joint_7_rad",
+        # right hand
+        "right_hand_joint_1_rad",
+        "right_hand_joint_2_rad",
+        "right_hand_joint_3_rad",
+        "right_hand_joint_4_rad",
+        "right_hand_joint_5_rad",
+        "right_hand_joint_6_rad",
+        "right_hand_joint_7_rad",
+    ]
     action_gripper_lerobot_names = state_gripper_lerobot_names
 
     def get_mjcf_gripper_joint_data(self, lerobot_gripper_data: list[float]) -> list[float]:
         return []
 
 
-class UnitreeG1Dof27ThreeFingerHandConfig(LerobotSimReplayConfig):
-    mjcf_path = Path(__file__).parent / "mjcfs/unitree_g1/g1_29dof_with_three_finger_hand.xml"
+class UnitreeG1Dof29FiveFingerHandConfig(LerobotSimReplayConfig):
+    mjcf_path = Path(__file__).parent / "mjcfs/unitree_g1/g1_29dof.xml"
 
     left_eef_mjcf_site_name: str = "left_eef_site"
     right_eef_mjcf_site_name: str = "right_eef_site"
@@ -119,22 +136,6 @@ class UnitreeG1Dof27ThreeFingerHandConfig(LerobotSimReplayConfig):
         "right_wrist_roll_joint",
         "right_wrist_pitch_joint",
         "right_wrist_yaw_joint",
-        # left_hand
-        "left_hand_thumb_0_joint",
-        "left_hand_thumb_1_joint",
-        "left_hand_thumb_2_joint",
-        "left_hand_middle_0_joint",
-        "left_hand_middle_1_joint",
-        "left_hand_index_0_joint",
-        "left_hand_index_1_joint",
-        # right_hand
-        "right_hand_thumb_0_joint",
-        "right_hand_thumb_1_joint",
-        "right_hand_thumb_2_joint",
-        "right_hand_index_0_joint",
-        "right_hand_index_1_joint",
-        "right_hand_middle_0_joint",
-        "right_hand_middle_1_joint",
     ]
     action_arm_joint_mjcf_names = state_arm_joint_mjcf_names
     state_arm_joint_lerobot_names: list[str] = [
@@ -154,6 +155,15 @@ class UnitreeG1Dof27ThreeFingerHandConfig(LerobotSimReplayConfig):
         "right_arm_joint_5_rad",
         "right_arm_joint_6_rad",
         "right_arm_joint_7_rad",
+    ]
+
+    action_arm_joint_lerobot_names = state_arm_joint_lerobot_names
+
+    # gripper names allow not 1v1 assignment
+    state_gripper_joint_mjcf_names: list[str] = []
+    action_gripper_joint_mjcf_names = state_gripper_joint_mjcf_names
+
+    state_gripper_lerobot_names: list[str] = [
         # left_hand
         "left_hand_joint_1_rad",
         "left_hand_joint_2_rad",
@@ -169,14 +179,6 @@ class UnitreeG1Dof27ThreeFingerHandConfig(LerobotSimReplayConfig):
         "right_hand_joint_5_rad",
         "right_hand_joint_6_rad",
     ]
-
-    action_arm_joint_lerobot_names = state_arm_joint_lerobot_names
-
-    # gripper names allow not 1v1 assignment
-    state_gripper_joint_mjcf_names: list[str] = []
-    action_gripper_joint_mjcf_names = state_gripper_joint_mjcf_names
-
-    state_gripper_lerobot_names: list[str] = []
     action_gripper_lerobot_names = state_gripper_lerobot_names
 
     def get_mjcf_gripper_joint_data(self, lerobot_gripper_data: list[float]) -> list[float]:
