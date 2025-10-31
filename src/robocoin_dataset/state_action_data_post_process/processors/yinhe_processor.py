4b30bd9019a90dfcd9a0efa87b261655fd2a0261
baai-magma <madmaliu@bupt.edu.cn>
2025-10-31 20:35:12 +0800

    tmp

---
+++ src/robocoin_dataset/state_action_data_post_process/processors/yinhe_processor.py
@@ -0,0 +1,36 @@
+from pathlib import Path
+
+import numpy as np
+
+from .state_action_data_processor_base import StateActionDataPostProcessorBase
+
+
+class YinheProcessor(StateActionDataPostProcessorBase):
+    def __init__(self, convert_path: str | Path) -> None:
+        super().__init__(convert_path)
+
+    def prepare_processing(self) -> None:
+        pass
+
+    def _smooth_gripper_open_data(self, data: np.ndarray) -> np.ndarray:
+        smoothed = data.copy()
+        return smoothed
+
+    # 该方法将ori_state_data进行后处理，返回结果为后处理后的数据
+    def process_episode_state_data(self, ori_state_data: np.ndarray) -> np.ndarray:
+        new_state_data = ori_state_data.copy()
+        return new_state_data
+
+    # 该方法将ori_action_data进行后处理，返回结果为后处理后的数据
+    def process_episode_action_data(self, ori_action_data: np.ndarray) -> np.ndarray:
+        new_action_data = ori_action_data.copy()
+        return new_action_data
+    def get_modified_feature_names(self):
+        return super().get_modified_feature_names()
+
+    def get_modified_info_state_names(self) -> dict[str, str]:
+        return {}
+
+    # 该方法返回处理后的action数据名称
+    def get_modified_info_action_names(self) -> dict[str, str]:
+        return {}
