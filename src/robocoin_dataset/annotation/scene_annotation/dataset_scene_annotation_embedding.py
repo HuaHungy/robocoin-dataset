fc769972a90ff4aef163df672ed4d5598b7ff159
PINKCONCRETE <1552738325@qq.com>
2025-11-05 11:44:25 +0800

    fix ""

--- src/robocoin_dataset/annotation/scene_annotation/dataset_scene_annotation_embedding.py
+++ src/robocoin_dataset/annotation/scene_annotation/dataset_scene_annotation_embedding.py
@@ -46,7 +46,7 @@ class SceneAnnotationDataPostProcessor(DataPostProcessorBase):
         scene_jsonl_file_path.parent.mkdir(parents=True, exist_ok=True)
         with open(scene_jsonl_file_path, "w") as f:
             for i, annotation in enumerate(self.scene_annotations):
-                f.write(f'"scene_index": {i}, "scene": "{annotation}"\n')
+                f.write(f'{{"scene_index": {i}, "scene": "{annotation}"}}\n')
 
 
 class SceneAnnotationEmbedding:
