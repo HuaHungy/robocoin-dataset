8b6bd9db41836f1772fe7a3788c1e486cc25ae40
baai-magma <madmaliu@bupt.edu.cn>
2025-11-11 22:24:54 +0800

    fix sa_dpp and replay config

--- src/robocoin_dataset/sim_replay/configs/agilex_cobot_magic_config.py
+++ src/robocoin_dataset/sim_replay/configs/agilex_cobot_magic_config.py
@@ -409,7 +409,7 @@ class AgilexCobotMagicLerobotRealsenseSimReplayConfig(LerobotSimReplayConfig):
     state_gripper_lerobot_names: list[str] = ["left_gripper_open", "right_gripper_open"]
     action_gripper_lerobot_names = state_gripper_lerobot_names
 
-    gripper_open_max = 10
+    gripper_open_max = 0.8
     gripper_open_min = 0
     gripper_joint_max = 0.04
     gripper_joint_min = 0
