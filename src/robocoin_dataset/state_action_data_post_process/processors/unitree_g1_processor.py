6c96bae68911bae8fa0c85ef5ed292afea26d57c
baai-magma <madmaliu@bupt.edu.cn>
2025-11-13 11:25:48 +0800

    fix replay_config

--- src/robocoin_dataset/state_action_data_post_process/processors/unitree_g1_processor.py
+++ src/robocoin_dataset/state_action_data_post_process/processors/unitree_g1_processor.py
@@ -237,28 +237,28 @@ class UnitreeG1ThreeFingerOutProcessor(StateActionDataPostProcessorBase):
     ]
 
 
-class UnitreeG1FiveFingerHandProcessor(StateActionDataPostProcessorBase):
-    def __init__(self, convert_path: str | Path) -> None:
-        super().__init__(convert_path)
+# class UnitreeG1FiveFingerHandProcessor(StateActionDataPostProcessorBase):
+#     def __init__(self, convert_path: str | Path) -> None:
+#         super().__init__(convert_path)
 
-    def prepare_processing(self) -> None:
-        pass
+#     def prepare_processing(self) -> None:
+#         pass
 
-    # 该方法将ori_state_data进行后处理，返回结果为后处理后的数据
-    def process_episode_state_data(self, ori_state_data: np.ndarray) -> np.ndarray:
-        return ori_state_data.copy()
+#     # 该方法将ori_state_data进行后处理，返回结果为后处理后的数据
+#     def process_episode_state_data(self, ori_state_data: np.ndarray) -> np.ndarray:
+#         return ori_state_data.copy()
 
-    # 该方法将ori_action_data进行后处理，返回结果为后处理后的数据
-    def process_episode_action_data(self, ori_action_data: np.ndarray) -> np.ndarray:
-        return ori_action_data.copy()
+#     # 该方法将ori_action_data进行后处理，返回结果为后处理后的数据
+#     def process_episode_action_data(self, ori_action_data: np.ndarray) -> np.ndarray:
+#         return ori_action_data.copy()
 
-    # 该方法返回处理后的state数据名称
-    def get_modified_feature_names(self):
-        return super().get_modified_feature_names()
+#     # 该方法返回处理后的state数据名称
+#     def get_modified_feature_names(self):
+#         return super().get_modified_feature_names()
 
-    def get_modified_info_state_names(self) -> dict[str, str]:
-        return {}
+#     def get_modified_info_state_names(self) -> dict[str, str]:
+#         return {}
 
-    # 该方法返回处理后的action数据名称
-    def get_modified_info_action_names(self) -> dict[str, str]:
-        return {}
+#     # 该方法返回处理后的action数据名称
+#     def get_modified_info_action_names(self) -> dict[str, str]:
+#         return {}
